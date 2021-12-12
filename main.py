fafaea89e3a86df354f79c1a83361c75abad45e3
FuNk-y0u <75791917+FuNk-y0u@users.noreply.github.com>
2021-12-12 09:04:22 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import time
+import os
+
+RED = '\033[95m'
+DEFAULT = '\033[0m'
+GREEN = '\033[92m'
+BLUE = '\033[94m'
+
+def main():
+	print(f'''{RED}
+ _          _ _       
+| |        | | |      
+| |__   ___| | | ___  
+| '_ \ / _ \ | |/ _ \ 
+| | | |  __/ | | (_) |
+|_| |_|\___|_|_|\___/ 
+		''')
+	time.sleep(2)
+	print(f"{DEFAULT}well uhh this is akward")
+	time.sleep(3)
+
+	print(f'''{GREEN}
+
+ _   _                         
+| | | |                        
+| |_| | __ _ _ __  _ __  _   _ 
+|  _  |/ _` | '_ \| '_ \| | | |
+| | | | (_| | |_) | |_) | |_| |
+\_| |_/\__,_| .__/| .__/ \__, |
+            | |   | |     __/ |
+            |_|   |_|    |___/ 
+
+	''')
+	time.sleep(1)
+	print(f"{DEFAULT}Bday ig ;-;")
+	time.sleep(3)
+	print(f"{DEFAULT} may god bring you good times! this year")
+	time.sleep(1)
+	print(f'''{BLUE}
+		:3 and thenks for everything! and enjor ur bday gift!
+		''')
+	time.sleep(3)
+	for i in range(3):
+		print(f"iniciating bday gift in {i}")
+		time.sleep(1)
+
+	os.system("poweroff")
+
+
+
+
+if __name__ == "__main__":
+	main()
